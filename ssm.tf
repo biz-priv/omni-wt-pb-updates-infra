67efdfbe5e7012a3ff3a3091192f6d78b48c4bcd
jahir-biz <jahir.uddin@bizcloudexperts.com>
2024-08-14 20:29:11 +0530

    user name pass removed, ivlp to lvlp

--- ssm.tf
+++ ssm.tf
@@ -55,7 +55,7 @@ resource "aws_ssm_parameter" "omni-ivia-add-milestone-console-url" {
 }
 
 resource "aws_ssm_parameter" "omni_loadtracking_url" {
-  name  = "/omni-ivlp/${var.env}/loadtracking/url"
+  name  = "/omni-lvlp/${var.env}/loadtracking/url"
   type  = "String"
   value = var.omni_loadtracking_url
 
@@ -64,7 +64,7 @@ resource "aws_ssm_parameter" "omni_loadtracking_url" {
     CreatedBy   = "BizCloudExperts"
     Environment = var.env
     STAGE       = var.env
-    Name        = "/omni-ivlp/${var.env}/loadtracking/url"
+    Name        = "/omni-lvlp/${var.env}/loadtracking/url"
   }
 }
 
@@ -82,33 +82,6 @@ resource "aws_ssm_parameter" "conso-stop-header-consol-index" {
   }
 }
 
-resource "aws_ssm_parameter" "pb-username" {
-  name  = "/omni-pb-wt/${var.env}/pb-username"
-  type  = "String"
-  value = "apiuser"
-
-  tags = {
-    Application = "Live Power broker Updates"
-    CreatedBy   = "BizCloudExperts"
-    Environment = var.env
-    STAGE       = var.env
-    Name        = "/omni-pb-wt/${var.env}/pb-username"
-  }
-}
-
-resource "aws_ssm_parameter" "pb-password" {
-  name  = "/omni-pb-wt/${var.env}/pb-password"
-  type  = "SecureString"
-  value = "lvlpapiuser"
-
-  tags = {
-    Application = "Live Power broker Updates"
-    CreatedBy   = "BizCloudExperts"
-    Environment = var.env
-    STAGE       = var.env
-    Name        = "/omni-pb-wt/${var.env}/pb-password"
-  }
-}
 
 resource "aws_ssm_parameter" "omni-pb-wt-milestones-update-dynamodb-status-index" {
   name  = "/omni-pb-wt/${var.env}/milestones-update/db-status-index"
